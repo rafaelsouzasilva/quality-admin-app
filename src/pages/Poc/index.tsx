c7773eaa50635ad706817cb52ff47eea89c0e7b7
Rafael Souza da Silva <rafael.souza.silva@hotmail.com.br>
2021-02-01 00:33:17 -0300

    Fix link youtube

--- src/pages/Poc/index.tsx
+++ src/pages/Poc/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const link = 'https://www.youtube.com/watch?v=Q2LDobhGHm4';
+const link = 'https://www.youtube.com/watch?v=tf8ehLMORVQ';
 
 function redirect() {
      window.location.href = link;
